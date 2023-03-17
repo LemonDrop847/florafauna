2702b6e6f5cc3b76eaeeafccd6ab21906688a5ec
LemonDrop847 <nitinmishra847@gmail.com>
2023-03-17 23:35:29 +0530

    CreatePost, Logo

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,12 +1,18 @@
+import { Link } from "react-router-dom";
+import SignIn from "../services/auth/signIn";
+import SignUp from "../services/auth/signUp";
+
 const Home = () => {
-    return ( 
-        <div>
-            <h1>Hello</h1>
-            <p>
-                This is flora Fauna
-            </p>
-        </div>
-     );
-}
- 
+  return (
+    <div>
+      <h1>Hello</h1>
+      <p>This is flora Fauna</p>
+      <SignUp />
+      <SignIn />
+
+      <Link to="/CreatePost">Post Page</Link>
+    </div>
+  );
+};
+
 export default Home;
