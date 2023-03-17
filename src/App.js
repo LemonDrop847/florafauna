d1742b735a979cbaeea8d2c9fdf009334e8317cb
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-03-18 00:43:46 +0530

    home page changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 // import logo from './logo.svg';
 import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/navbar';
 import Home from './pages/homePage';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <Router>
+      <Navbar/>
         <Routes>
           <Route path="/" element={<Home/>}/>
+          
         </Routes>
       </Router>
     </div>
