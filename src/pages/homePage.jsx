64fb958f008edfc52c02b46a51af668419670b19
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-03-17 20:49:07 +0530

    one time changes

---
+++ src/pages/homePage.jsx
@@ -0,0 +1,12 @@
+const Home = () => {
+    return ( 
+        <div>
+            <h1>Hello</h1>
+            <p>
+                This is flora Fauna
+            </p>
+        </div>
+     );
+}
+ 
+export default Home;
