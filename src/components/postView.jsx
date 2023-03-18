0bfc63f1a7cec1840c3970633424f985ebcfd43d
LemonDrop847 <nitinmishra847@gmail.com>
2023-03-18 17:12:56 +0530

    Update

--- src/components/postView.jsx
+++ src/components/postView.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import PostDetails from "./postDetails";
+import Popup from "./popUp";
 
 function Post({ post }) {
   const [showDetails, setShowDetails] = useState(false);
@@ -11,12 +12,19 @@ function Post({ post }) {
   return (
     <div onClick={handlePostClick}>
       {post.images && post.images.length > 0 && (
-        <img src={post.images[0]} alt="Post" style={{ maxWidth: "100%", maxHeight: "300px" }} />
+        <img
+          src={post.images[0]}
+          alt="Post"
+          style={{ maxWidth: "100%", maxHeight: "300px" }}
+        />
       )}
       <h2>{post.name}</h2>
       <p>{post.location}</p>
       <p>By {post.username}</p>
-      {showDetails && <PostDetails post={post} onClose={() => setShowDetails(false)} />}
+      
+      {showDetails && (
+        <PostDetails post={post} onClose={() => setShowDetails(false)} />
+      )}
     </div>
   );
 }
