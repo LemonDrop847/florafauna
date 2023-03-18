363314b1c3f22eca31e056d5b226c8641ab73a46
Tanisha Pattnaik <tanishakhuc@gmail.com>
2023-03-18 11:25:49 +0530

    edit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 // import logo from './logo.svg';
 import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
 import './App.css';
-<<<<<<< HEAD
 import About from './pages/about';
-=======
 import Navbar from './components/navbar';
->>>>>>> a7432f54211b75f26ee46a9a99493e000992a8ae
 import Home from './pages/homePage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CreatePost from './pages/createPost';
@@ -17,12 +14,9 @@ function App() {
       <Navbar/>
         <Routes>
           <Route path="/" element={<Home/>}/>
-<<<<<<< HEAD
           <Route exact path="/about" element={<About/>}/>
-=======
           
           <Route path="/CreatePost" element={<CreatePost/>}/>
->>>>>>> a7432f54211b75f26ee46a9a99493e000992a8ae
         </Routes>
       </Router>
     </div>
